9fae0f183c6641ebe2849847129f561048357880
yanggt <yanggt@163.com>
2019-05-31 18:50:52 +0800

    ..

--- src/main/java/com/ygt/jwtjwks/controller/JoseController.java
+++ src/main/java/com/ygt/jwtjwks/controller/JoseController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.util.UUID;
 
 /**
@@ -33,7 +34,10 @@ public class JoseController {
     @Value("${jwt.subject}")
     private String subject;
 
-    RsaJsonWebKey rsaJsonWebKey;
+    @Value("${jwt.expirationTime}")
+    private String expirationTime;
+
+    private RsaJsonWebKey rsaJsonWebKey;
 
     {
         try {
@@ -45,12 +49,19 @@ public class JoseController {
 
     @GetMapping("/jwks.json")
     public String jwks(HttpServletRequest request) {
-        System.out.println("有请求。。。");
+        System.out.println("有请求...");
         System.out.println(request.getRemoteAddr());
         // 设置kid
-        rsaJsonWebKey.setKeyId(String.valueOf(UUID.randomUUID()));
-        System.out.println("jwks:" + rsaJsonWebKey.toJson());
-        return rsaJsonWebKey.toJson();
+        rsaJsonWebKey.setKeyId("poid087fd-1629-4cf6-9576-2aad27b94a64");
+        File file = new File("./rsa.dat");
+        if (!file.exists()) {
+            writeObjectToFile(rsaJsonWebKey);
+        } else {
+            rsaJsonWebKey = readObjectFromFile();
+        }
+        System.out.println("jwks.json:----"+"{\"keys\":[" + rsaJsonWebKey.toJson() + "]}");
+
+        return "{\"keys\":[" + rsaJsonWebKey.toJson() + "]}";
     }
 
     @GetMapping("/token")
@@ -59,9 +70,11 @@ public class JoseController {
         // 设置body部分
         JwtClaims claims = new JwtClaims();
         claims.setIssuer(issuer);
-        claims.setExpirationTime(NumericDate.fromSeconds(4685989700L));
-        claims.setIssuedAt(NumericDate.fromSeconds(1532389700L));
+        long expTime = getExpirationTime(expirationTime);
+        claims.setExpirationTime(NumericDate.fromMilliseconds(System.currentTimeMillis() + expTime));
+        claims.setIssuedAt(NumericDate.fromMilliseconds(System.currentTimeMillis()));
         claims.setSubject(subject);
+        claims.setAudience("Audience");
 //        claims.setClaim("email","mail@example.com");
 //        List<String> groups = Arrays.asList("group-one", "other-group", "group-three");
 //        claims.setStringListClaim("groups", groups);
@@ -91,15 +104,88 @@ public class JoseController {
         return jwt;
     }
 
+    /**
+     * 转换过期时间为ms
+     *
+     * @param expirationTime 过期时间
+     * @return 过期时间的ms值
+     */
+    private long getExpirationTime(String expirationTime) {
+        String num = expirationTime.substring(0, expirationTime.length() - 1);
+        String unit = expirationTime.substring(expirationTime.length() - 1);
+        long expTime;
+        switch (unit) {
+            case "y":
+                expTime = Integer.valueOf(num) * 365 * 24 * 60 * 60 * 1000;
+                break;
+            case "d":
+                expTime = Integer.valueOf(num) * 24 * 60 * 60 * 1000;
+                break;
+            case "h":
+                expTime = Integer.valueOf(num) * 60 * 60 * 1000;
+                break;
+            case "m":
+                expTime = Integer.valueOf(num) * 60 * 1000;
+                break;
+            case "s":
+                expTime = Integer.valueOf(num) * 1000;
+                break;
+            default:
+                expTime = 100 * 365 * 24 * 60 * 60 * 1000;
+                break;
+        }
+        return expTime;
+    }
+
+    public void writeObjectToFile(Object obj) {
+        File file = new File("./rsa.dat");
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(file);
+            ObjectOutputStream objOut = new ObjectOutputStream(out);
+            objOut.writeObject(obj);
+            objOut.flush();
+            objOut.close();
+            System.out.println("write object success!");
+        } catch (IOException e) {
+            System.out.println("write object failed");
+            e.printStackTrace();
+        }
+    }
+
+    public RsaJsonWebKey readObjectFromFile() {
+        RsaJsonWebKey temp = null;
+        File file = new File("./rsa.dat");
+        FileInputStream in;
+        try {
+            in = new FileInputStream(file);
+            ObjectInputStream objIn = new ObjectInputStream(in);
+            temp = (RsaJsonWebKey) objIn.readObject();
+            objIn.close();
+            System.out.println("read object success!");
+        } catch (IOException e) {
+            System.out.println("read object failed");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return temp;
+    }
+
+    /**
+     * 验证token
+     *
+     * @param request request
+     */
     @GetMapping("/validate")
-    public void validate(HttpServletRequest request) {
+    public String validate(HttpServletRequest request) {
         String token = request.getParameter("token");
         JwtConsumer jwtConsumer = new JwtConsumerBuilder()
                 .setRequireExpirationTime() // jwt 必须有过期时间
                 .setAllowedClockSkewInSeconds(30) // allow some leeway in validating time based claims to account for clock skew
                 .setRequireSubject() // the JWT must have a subject claim
                 .setExpectedIssuer(issuer) // whom the JWT needs to have been issued by
-//                .setExpectedAudience("Audience") // to whom the JWT is intended for
+                .setExpectedAudience("Audience") // to whom the JWT is intended for
                 .setVerificationKey(rsaJsonWebKey.getKey()) // verify the signature with the public key
                 .setJwsAlgorithmConstraints( // only allow the expected signature algorithm(s) in the given context
                         new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST, // which is only RS256 here
@@ -110,6 +196,7 @@ public class JoseController {
             //  Validate the JWT and process it to the Claims
             JwtClaims jwtClaims = jwtConsumer.processToClaims(token);
             System.out.println("JWT 验证成功! " + jwtClaims);
+            return "token is ok";
         } catch (InvalidJwtException e) {
             System.out.println("不可用的 JWT! " + e);
 
@@ -119,6 +206,7 @@ public class JoseController {
                 } catch (MalformedClaimException e1) {
                     e1.printStackTrace();
                 }
+                return "jwt is expired";
             }
 
             // Or maybe the audience was invalid
@@ -128,7 +216,9 @@ public class JoseController {
                 } catch (MalformedClaimException e1) {
                     e1.printStackTrace();
                 }
+                return "JWT had wrong audience";
             }
         }
+        return "jwt error";
     }
 }
